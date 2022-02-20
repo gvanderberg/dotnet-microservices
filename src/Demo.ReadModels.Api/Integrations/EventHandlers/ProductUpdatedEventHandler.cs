f1461247ab53a6bc57dc0c044eaaea56904d4d8e
gvanderberg <gvanderberg@titansoftware.co.za>
2022-02-20 11:37:13 +0200

    add event handlers

---
+++ src/Demo.ReadModels.Api/Integrations/EventHandlers/ProductUpdatedEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Demo.Events.Products;
+
+using Microsoft.Extensions.Logging;
+
+using NServiceBus;
+
+namespace Demo.ReadModels.Api.Integrations.EventHandlers
+{
+    public class ProductUpdatedEventHandler : IHandleMessages<ProductUpdatedEvent>
+    {
+        private readonly ILogger<ProductUpdatedEventHandler> _logger;
+
+        public ProductUpdatedEventHandler(ILogger<ProductUpdatedEventHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Handle(ProductUpdatedEvent message, IMessageHandlerContext context)
+        {
+            _logger.LogInformation("Handle ProductUpdatedEvent");
+
+            throw new NotImplementedException();
+        }
+    }
+}
